98f2d6df65e4bd4aef79e36ccc6ec957f6c5dc1f
sivaramsj <sivaramsj003@gmail.com>
2023-12-21 23:27:30 +0530

    view specific student Details actions is create

--- student/views.py
+++ student/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Student
 
 # Create your views here.
 def index(request):  
     return render(request,'student/index.html',{'students':Student.objects.all()})
+
+def view_student(request,id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
