12976eb771bd80ca8077361fad8f150df4c1df31
sivaramsj <sivaramsj003@gmail.com>
2023-12-21 19:47:25 +0530

    Table is create using models

--- student/models.py
+++ student/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    student_no=models.PositiveIntegerField()
+    First_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=100)
+    fields_of_study=models.CharField(max_length=100)
+    gpa=models.FloatField()
+
+    def __str__(self):
+        return f"Student : {self.First_name} {self.last_name}"
+        
